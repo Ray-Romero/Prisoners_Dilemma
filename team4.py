1c4fc911c529aceb45ae258e68d3232a184974da
Ray Romero <46662592+Ray-Romero@users.noreply.github.com>
2019-02-20 13:52:22 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Goldfish.' # Only 10 chars displayed.
+strategy_name = 'Go Fish'
+strategy_description = 'We start off with Collude but adapt to our opponets moves.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
