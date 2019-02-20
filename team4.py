b91ce4ed4fe0221e5dcfe08c1f3db8c2c2722271
Ray Romero <46662592+Ray-Romero@users.noreply.github.com>
2019-02-20 14:07:52 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Goldfish.' # Only 10 chars displayed.
+strategy_name = 'The snack that smiles back.'
+strategy_description = 'We start with collude but adapt to our opponents moves.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,16 +17,27 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    if len(my_history)==0: # It's the first round: collude
+        return 'c'
+    else:
+        if their_score == 0:
+            return 'c'
+        else:
+    '''Make my move based on the history with this player.
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
     
-    return 'c'
+    Returns 'c' or 'b' for collude or betray.
+    '''
+            if 'b' in their_history:
+                return 'b'
+            else:
+                return 'c'
+    
+            return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
